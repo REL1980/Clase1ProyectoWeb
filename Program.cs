b954e6e0f38d02ea9ab5f012bd32d4e4c49d815f
REL1980 <rafaelandeau@hotmail.com>
2021-12-29 15:17:50 -0300

    nuevo commit web

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ app.MapGet("/api/users", () => {
     var user1 = new User("Enrique","Diaz",1980);
     var user2 = new User("Rodolfo","Perez",1975);
     var user3 = new User("Pablito","Peñaloza",1995);
+    var user4 = new User("Angel","Sancon",2004);
     
     var users = new List<User>();
 
     users.Add(user1);
     users.Add(user2);
     users.Add(user3);
+    users.Add(user4);
 
     return users;
 
